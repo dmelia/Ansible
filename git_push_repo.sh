8c4724c2f73a49534a018241bdaea281b0c6cb2c
SCHLOSSER Guilhem <guilhemETkarine@hotmail.fr>
2021-09-23 17:55:03 +0200

    Delete git_push_repo.sh

--- git_push_repo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Push an existant repository"
-echo "What is the orign repository name ?, exemple: https://github.com/geds3169/Ansible.git :"
-read name
-
-git remote add origin "$name"
-git branch -M main
-git push -u origin main
